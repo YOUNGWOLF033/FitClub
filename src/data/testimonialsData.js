7dffbc6f86e2e600be8079e9416b577a81712af5
unknown <samarth.jain2020@vitstudent.ac.in>
2023-07-23 22:42:23 +0530

    Initial commit

---
+++ src/data/testimonialsData.js
@@ -0,0 +1,25 @@
+import image1 from "../assets/t-image1.png";
+import image2 from "../assets/t-image2.jpg";
+import image3 from "../assets/t-image3.jpg";
+
+export const testimonialsData = [
+  {
+    image: image1,
+    review:
+      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
+    name: 'YASH GARG',
+    status : 'ENTREPRENEUR'
+  },
+  {
+    image: image2,
+    review: 'Here you will get all the help you need.Trust Fit Club ',
+    name: 'ARJUN GOEL',
+    status: 'COACH'
+  },
+  {
+    image : image3,
+    review:' I really feel grat after joining the Fit Club.The best choice I ever made in my life.',
+    name: 'DIYVANSH GOEL',
+    status: "CUSTOMER"
+  }
+];
